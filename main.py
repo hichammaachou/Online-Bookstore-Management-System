65100e33f589e23a68ab0aabcb0a70431cc650b6
hicham maachou <maachouhicham093@gmail.com>
2023-08-08 15:52:49 +0100

    Book class made

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+class Book:
+    def __init__(self, title, author, genre, price, availability):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.price = price
+        self.availability = availability
+
+
